b9de8b59018966a01cda27dbd2811c4e28acf0df
Junior Javier Leaño Torres <javierleano.25@gmail.com>
2020-08-11 21:42:41 -0300

    nuevo cambio test

--- dos.js
+++ dos.js
@@ -5,7 +5,68 @@ marca, precio, peso en kilogramos, tipo(sólido o líquido)
 a)informar el peso total de la compra.
 b)la marca del más caro de los líquidos
 c)la marca del más barato de los sólidos
-*/function mostrar()
-{
-  alert("dos");
+*/function mostrar() {
+  let producto;
+  let marca;
+  let precio;
+  let peso;
+  let tipo;
+  let seguir;
+  let acumPeso = 0;
+  let acumMarca = 0;
+  let marcaMasCaro;
+  let marcaMax;
+  let marcaMasbarato;
+  let contadorPeso = 0;
+  let acumPrecio = 0;
+  let precioMax;
+  let productoCaro;
+  let precioBarato;
+  let productoBarato;
+  do {
+
+    producto = prompt("Ingrese un producto");
+
+    marca = prompt("Ingrese Marca");
+
+
+    precio = prompt("Ingrese Precio");
+    while (precio < 1) {
+      precio = prompt("Ingrese Precio Mayor a 0");
+    }
+    if (acumPrecio == 0 || precio > precioMax){
+      precioMax = precio;
+      productoCaro = marca;
+    }
+
+    if (acumPrecio == 0 || precio > precioBarato ){
+      precioBarato = precio;
+      productoBarato = marca;
+      
+    }
+
+    peso = parseInt(prompt("Ingrese Peso en Kg"));
+    while (peso < 1) {
+      peso = prompt("Error Ingrese Peso Mayor a 0");
+    
+    }
+    tipo = prompt("El producto es solido O liquido ");
+    while (tipo != 'solido' && tipo != 'liquido') {
+      tipo = prompt("error El producto es solido O liquido ");
+
+    }
+
+    switch (marca) {
+      
+        
+
+    }
+
+    seguir = prompt("Desea Agregar Otro Producto? :");
+  } while (seguir == 's');
+
+  console.log("el peso total de la compra es : " + acumPeso);
+  console.log("la marca del más caro de los líquidos : " + productoBarato);
+  console.log("la marca del más barato de los sólidos : " + marcaMasbarato);
+
 }
--- uno.js
+++ uno.js
@@ -9,5 +9,57 @@ pedir datos por prompt y mostrar por document.write o console.log
 */
 function mostrar()
 {
-	alert("uno");
+	let nombre;
+	let peso;
+	let sexo;
+	let edad;
+	let acumMujeres = 0;
+	let contadorMujeres = 0;
+	let pesoMax;
+	let contador = 0;
+	let hombreMasPesado = 0;
+	let promedio;
+	let acumEdad = 0;
+	let contadorEdad = 0;
+
+
+	for (let i = 0; i < 5; i++){
+	nombre = prompt("Ingrese Nombre");
+	
+
+	peso = parseInt(prompt("Ingrese Peso valido"));
+	
+	sexo = prompt("Ingrese Sexo (f o m)");
+	while(sexo != 'f' && sexo != 'm'){
+	sexo = prompt("Error Ingrese Sexo (f o m)");
+	}
+	
+	edad = prompt("Ingrese edad");
+     if (edad > 0){ 
+	 }
+	
+	switch(sexo){
+		case 'f' :
+		contadorMujeres++;
+		if (edad > 0){
+			acumEdad++;
+		}
+		break;
+		case 'm' : 
+		pesoMax = peso;
+		if (edad > 0){
+			acumEdad++;
+		}
+		break;
+	}	
+	}
+   
+	   promedio = acumEdad / 5;
+
+	console.log("la cantidad de mujeres son : " + contadorMujeres);
+	console.log("la edad promedio es " + promedio);
+	console.log("el hombre mas pesado " + pesoMax);
+
+
+	
 }
